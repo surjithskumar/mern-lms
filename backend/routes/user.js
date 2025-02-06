c09e41e824d9011a005339c190906a9187c1c6a4
SURJITH S KUMAR <surjithskumar23@gmail.com>
2025-02-06 12:18:10 +0530

    course progress

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { forgotPassword, loginUser, myProfile, register, resetPassword, verifyUser } from '../controllers/user.js';
 import { isAuth } from '../middlewares/isAuth.js';
+import { addProgress, getYourProgress } from '../controllers/course.js';
 
 const router = express.Router();
 
@@ -15,5 +16,9 @@ router.post('/user/me',isAuth, myProfile);
 router.post('/user/forgot',forgotPassword);
 
 router.post('/user/reset',resetPassword);
+
+router.post('/user/progress',isAuth,addProgress);
+
+router.get('/user/progress',isAuth,getYourProgress);
 
 export default router;
