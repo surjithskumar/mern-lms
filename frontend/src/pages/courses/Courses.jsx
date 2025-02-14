9f7b87fdcd16a514748bdabd597d262829654e59
SURJITH S KUMAR <surjithskumar23@gmail.com>
2025-02-14 12:25:08 +0530

    bug fix

--- frontend/src/pages/courses/Courses.jsx
+++ frontend/src/pages/courses/Courses.jsx
@@ -9,8 +9,7 @@ const Courses = () => {
   console.log(courses);
   return (
     <div className="courses">
-      <h2>Available Courses</h2>
-
+      <h2>All Courses</h2>
       <div className="course-container">
         {courses && courses.length > 0 ? (
           courses.map((e) => <CourseCard key={e._id} course={e} />)
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,21 +2,34 @@ import React from "react";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
 import Testimonials from "../../components/testimonials/Testimonials";
+import CourseCard from "../../components/coursecard/CourseCard";
+import { CourseData } from "../../context/CourseContext";
 
 const Home = () => {
   const navigate = useNavigate();
+  const { courses } = CourseData();
+
   return (
     <div>
       <div className="home">
         <div className="home-content">
-          <h1>Welcome to our E-learning Platform</h1>
-          <p>Learn, Grow, Excel</p>
-          <button onClick={() => navigate("/courses")} className="common-btn">
-            Get Started
-          </button>
+          <h1>Learn From Anywhere, Anytime</h1>
+          <p>Be a part of Next-Gen Learning</p>
         </div>
+        <div className="courses">
+        <div className="course-container">
+          {courses && courses.length > 0 ? (
+            courses.map((e) => <CourseCard key={e._id} course={e} />)
+          ) : (
+            <p>No Courses Yet!</p>
+          )}
+        </div>
+        </div>
+        <button onClick={() => navigate("/courses")} className="common-btn">
+            Explore More Courses
+          </button>
       </div>
-      <Testimonials/>
+      <Testimonials />
     </div>
   );
 };
